2581f39c8f2942e09a818ce13735ca6bbb8afdee
Alex1OK1 <95971681+Alex1OK1@users.noreply.github.com>
2024-12-25 14:22:28 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    public class Levenshtine
+    {
+        public static int Minimum(int a, int b, int c) => (a = a < b ? a : b) < c ? a : c;
+
+        public static int LevenshteinDistance(string firstWord, string secondWord)
+        {
+            int n = firstWord.Length + 1;
+            int m = secondWord.Length + 1;
+            int[,] matrixD = new int[n, m];
+
+            for (var i = 0; i < n; i++)
+            {
+                matrixD[i, 0] = i;
+            }
+
+            for (var j = 0; j < m; j++)
+            {
+                matrixD[0, j] = j;
+            }
+           /* for (var i = 0; i < n; i++)
+            {
+                for (var j = 0; j < m; j++)
+                {
+                    Console.Write(matrixD[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }*/
+            for (var i = 1; i < n; i++)
+            {
+                for (var j = 1; j < m; j++)
+                {
+                    var substitutionCost = firstWord[i - 1] == secondWord[j - 1] ? 0 : 1;
+                    //Console.WriteLine(firstWord[i - 1]);
+                    //Console.WriteLine(secondWord[j - 1]);
+                    //Console.WriteLine("-");
+                    matrixD[i, j] = Minimum(matrixD[i - 1, j] + 1,         
+                                            matrixD[i, j - 1] + 1,        
+                                            matrixD[i - 1, j - 1] + substitutionCost);
+                }
+            }
+
+                    return matrixD[n - 1, m - 1];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab5;
+
+namespace Lab5ShowCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Первое слово: ");
+            string s1 = Console.ReadLine();
+            Console.Write("Второе слово: ");
+            string s2 = Console.ReadLine();
+            //Console.WriteLine(s1);
+            //Console.WriteLine(s2);
+            //Console.WriteLine("_______");
+
+
+            Console.WriteLine("Расстояние Левенштейна: {0}", Lab5.Levenshtine.LevenshteinDistance(s1, s2));
+            Console.ReadLine();
+        }
+    }
+}
